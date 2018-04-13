3783c47f4d3dc38282ba6fbb0adbb6218d3b2dfb
Eli Asor <tcfh.ea@gmail.com>
2018-04-13 18:47:50 +0300

    Some changes to small_convnet.py

--- cnn_finetune/small_convnet.py
+++ cnn_finetune/small_convnet.py
@@ -3,13 +3,13 @@ from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
 
 
-def binary_convnet_model(img_rows, img_cols, channels=1, metrics=None):
+def binary_convnet_model(img_rows, img_cols, channels=3, metrics=None):
     model = Sequential()
-    model.add(Conv2D(32, (3, 3), input_shape=(img_rows, img_cols, channels)))
+    model.add(Conv2D(128, (3, 3), input_shape=(img_rows, img_cols, channels)))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
 
-    model.add(Conv2D(32, (3, 3)))
+    model.add(Conv2D(512, (3, 3)))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
 
@@ -18,7 +18,7 @@ def binary_convnet_model(img_rows, img_cols, channels=1, metrics=None):
     model.add(MaxPooling2D(pool_size=(2, 2)))
 
     model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
-    model.add(Dense(64))
+    model.add(Dense(32))
     model.add(Activation('relu'))
     model.add(Dropout(0.5))
     model.add(Dense(1))
